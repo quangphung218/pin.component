70b3e9e36c3d3eabda6770ba2d7cb246cf45235f
Quang Phung <quangphung91@gmail.com>
2026-05-14 13:30:07 +0700

    VALIDATION: `Validate.visibility` static method exists -> FAIL

---
+++ test/unit/pin.validation.test.js
@@ -0,0 +1,14 @@
+import { Validate, State } from "@quangphung218/pin";
+validation(State.VISIBILITY, () => {
+    given("Validate imported", () => {
+        then("Validate is defined", () => {
+            expect(Validate).toBeDefined();
+        });
+        and("Validate is defined", () => {
+            then("`Validate.visibility` static method is defined", () => {
+                expect(Validate.visibility).toBeDefined();
+            });
+        });
+    });
+});
+//# sourceMappingURL=pin.validation.test.js.map
--- test/unit/pin.validation.test.ts
+++ test/unit/pin.validation.test.ts
@@ -1,1 +1,15 @@
-import { Validate } from "@quangphung218/pin";
+import { Validate, Visibility, State } from "@quangphung218/pin";
+
+validation(State.VISIBILITY, () => {
+  given("Validate imported", () => {
+    then("Validate is defined", () => {
+      expect(Validate).toBeDefined();
+    });
+
+    and("Validate is defined", () => {
+      then("`Validate.visibility` static method is defined", () => {
+        expect(Validate.visibility).toBeDefined();
+      });
+    });
+  });
+});
