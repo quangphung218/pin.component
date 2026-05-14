41ea7bb69f431e42ce7451e58a991efa4536a9fe
Quang Phung <quangphung91@gmail.com>
2026-05-14 13:22:48 +0700

    STATE: `pin.visibility` getter exists -> FAIL

---
+++ test/unit/pin.test.js
@@ -0,0 +1,158 @@
+import { Template } from "@scalable.software/component";
+import { Pin, Tag, CSS, Attributes, State } from "@quangphung218/pin";
+// Configuration
+configuration(Configuration.TAG, () => {
+    and("Pin imported", () => {
+        then("Pin is defined", () => {
+            expect(Pin).toBeDefined();
+        });
+        and("Pin is defined", () => {
+            then("Pin.Tag static getter is defined", () => {
+                expect(Pin.Tag).toBeDefined();
+            });
+            and("Pin.Tag static getter is defined", () => {
+                then("Pin.Tag is Tag", () => {
+                    expect(Pin.Tag).toBe(Tag);
+                });
+            });
+        });
+    });
+});
+configuration(Configuration.ATTRIBUTES, () => {
+    and("Pin imported", () => {
+        then("Pin is defined", () => {
+            expect(Pin).toBeDefined();
+        });
+        and("Pin is defined", () => {
+            then("Pin.Attributes static getter is defined", () => {
+                expect(Pin.Attributes).toBeDefined();
+            });
+            and("Pin.Attributes static getter is defined", () => {
+                then("Pin.Attributes is Attributes", () => {
+                    expect(Pin.Attributes).toBe(Attributes);
+                });
+            });
+        });
+    });
+});
+// Utilities
+utility(Utilities.TEMPLATE, () => {
+    then("Pin.Template static property is defined", () => {
+        expect(Pin.Template).toBeDefined();
+    });
+    and("Pin.Template static property is defined", () => {
+        then("Pin.Template is a Template", () => {
+            expect(Pin.Template).toBeInstanceOf(Template);
+        });
+    });
+});
+// Compositions
+composition(Composition.TEMPLATE, () => {
+    given("Pin is defined in custom element registry", () => {
+        beforeEach(() => {
+            define(Pin.Tag, Pin);
+        });
+        and("HTML Template is added to DOM", () => {
+            let template;
+            beforeEach(async () => {
+                template = (await Pin.Template.load("pin.template.html"));
+            });
+            afterEach(() => {
+                remove(Pin.Tag);
+            });
+            then("template is defined", () => {
+                expect(template).toBeDefined();
+            });
+            and("a new pin is added to DOM", () => {
+                let pin;
+                beforeEach(() => {
+                    pin = add(Pin.Tag);
+                });
+                afterEach(() => {
+                    pin.remove();
+                });
+                then("pin.root contents contains template contents", () => {
+                    expect(pin.root.innerHTML).toContain(template.innerHTML);
+                });
+                then("`pin.root` contains `div.icon`", () => {
+                    const div = pin.root.querySelector("div.icon");
+                    expect(div).not.toBeNull();
+                });
+                when("`pin.root` contains `div.icon`", () => {
+                    let div;
+                    beforeEach(() => {
+                        div = pin.root.querySelector("div.icon");
+                    });
+                    then("`div.icon` contains `svg.unpinned`", () => {
+                        const svg = div.querySelector("svg.unpinned");
+                        expect(svg).not.toBeNull();
+                    });
+                });
+            });
+        });
+    });
+});
+composition(Composition.CSS, () => {
+    given("Pin is defined in custom element registry", () => {
+        beforeEach(() => {
+            define(Pin.Tag, Pin);
+        });
+        and("HTML Template is added to DOM", () => {
+            beforeEach(async () => {
+                await Pin.Template.load("pin.template.html");
+            });
+            afterEach(() => {
+                remove(Pin.Tag);
+            });
+            and("a new pin is added to DOM", () => {
+                let pin;
+                beforeEach(() => {
+                    pin = add(Pin.Tag);
+                });
+                afterEach(() => {
+                    pin.remove();
+                });
+                then("pin.root contents contains a link to stylesheet", () => {
+                    expect(pin.root.innerHTML).toContain("stylesheet");
+                });
+                and("pin.root contents contains a link to stylesheet", () => {
+                    then("the stylesheet file's name is correct", () => {
+                        expect(pin.root.innerHTML).toContain(CSS);
+                    });
+                });
+            });
+        });
+    });
+});
+// State
+state(State.VISIBILITY, () => {
+    given("Pin is defined in custom element registry", () => {
+        beforeEach(() => {
+            define(Pin.Tag, Pin);
+        });
+        and("HTML Template is added to DOM", () => {
+            beforeEach(async () => {
+                await Pin.Template.load("pin.template.html");
+            });
+            afterEach(() => {
+                remove(Pin.Tag);
+            });
+            and("a new pin is added to DOM", () => {
+                let pin;
+                beforeEach(() => {
+                    pin = add(Pin.Tag);
+                });
+                afterEach(() => {
+                    pin.remove();
+                });
+                then("`pin.visibility` getter exists", () => {
+                    expect(pin.visibility).toBeDefined();
+                });
+            });
+        });
+    });
+});
+// Operation
+// Event
+// Gesture
+//# sourceMappingURL=pin.test.js.map
